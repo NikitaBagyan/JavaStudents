181b8a98e97f35f270a528c5cbe8add86324d740
Nikita <doarchik@gmail.com>
2023-01-09 11:35:33 +0300

    New quesions

---
+++ sadao/Rifleman.java
@@ -0,0 +1,13 @@
+package sadao;
+
+public class Rifleman extends Soldier {
+    public Rifleman (String name) {
+        super(name);
+        this.price = 205;
+        this.phrase = "Мушкет и жену не дам никому";
+    }
+    @Override
+    public void attack(){
+        System.out.println(this.name + "аткаует из ружья");
+    }
+}
